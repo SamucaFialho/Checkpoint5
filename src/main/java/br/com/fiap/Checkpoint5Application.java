fa56a0ebd60bf5b83b9a484fb87ac252dc3cb952
SamucaFialho <samuel100fialho@gmail.com>
2025-10-23 00:44:42 -0300

    feat: add CiCddd

--- src/main/java/br/com/fiap/Checkpoint5Application.java
+++ src/main/java/br/com/fiap/Checkpoint5Application.java
@@ -2,7 +2,7 @@ package br.com.fiap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class Checkpoint5Application {
 
